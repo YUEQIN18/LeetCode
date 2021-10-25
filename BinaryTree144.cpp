dcbd270cf2fb0fc575214972c622c3e07b4ca485
YUEQIN18 <qinyue12345@gmail.com>
2021-10-25 01:55:58 -0400

    10/24

---
+++ BinaryTree144.cpp
@@ -0,0 +1,33 @@
+#include <stddef.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+  void traversal(TreeNode *cur, vector<int> &result)
+  {
+    if (cur == NULL)
+      return;
+    result.push_back(cur->val);
+    traversal(cur->left, result);
+    traversal(cur->right, result);
+  }
+
+  vector<int> preorderTraversal(TreeNode *root)
+  {
+    vector<int> result;
+    traversal(root, result);
+    return result;
+  }
+};
--- StackandQueue71.cpp
+++ StackandQueue71.cpp
@@ -11,16 +11,20 @@ public:
     int start = 1;
     for (int i = 1; i <= path.length(); ++i)
     {
+      // 遇到/时才进行判断
       if (i == path.length() || path[i] == '/')
       {
+        // 分割字符串
         string p = path.substr(start, i - start);
         if (p == "/")
           continue;
+        // 遇到'..'
         if (p == "..")
         {
           if (!s.empty())
             s.pop();
         }
+        // 遇到'.'不push，其他的都push
         else if (p.length() > 0 && p != ".")
         {
           s.push(p);
