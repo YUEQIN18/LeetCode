9bdc562681efb1815d4a23267b9270233f9d653f
YUEQIN18 <qinyue12345@gmail.com>
2021-10-27 02:20:49 -0400

    10/26

--- BinaryTree559.cpp
+++ BinaryTree559.cpp
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Node
@@ -36,5 +37,35 @@ public:
       depth = max(depth, maxDepth(root->children[i]));
     }
     return depth + 1;
+  }
+};
+
+// 另一种方法 层序遍历
+class solution
+{
+public:
+  int maxdepth(Node *root)
+  {
+    if (root == NULL)
+      return 0;
+    int depth = 0;
+    queue<Node *> que;
+    que.push(root);
+    while (!que.empty())
+    {
+      int size = que.size();
+      depth++; // 记录深度
+      for (int i = 0; i < size; i++)
+      {
+        Node *node = que.front();
+        que.pop();
+        for (int j = 0; j < node->children.size(); j++)
+        {
+          if (node->children[j])
+            que.push(node->children[j]);
+        }
+      }
+    }
+    return depth;
   }
 };
