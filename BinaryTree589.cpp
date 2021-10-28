abcfd8a1edd444442b165e60f5c2a7bdeafc5c6f
YUEQIN18 <qinyue12345@gmail.com>
2021-10-28 01:36:23 -0400

    10/27

---
+++ BinaryTree589.cpp
@@ -0,0 +1,46 @@
+#include <stddef.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val)
+  {
+    val = _val;
+  }
+
+  Node(int _val, vector<Node *> _children)
+  {
+    val = _val;
+    children = _children;
+  }
+};
+
+class Solution
+{
+public:
+  void traveral(Node *cur, vector<int> &result)
+  {
+    if (cur == NULL)
+      return;
+    result.push_back(cur->val);
+    for (auto n : cur->children)
+    {
+      traveral(n, result);
+    }
+  }
+
+  vector<int> preorder(Node *root)
+  {
+    vector<int> result;
+    traveral(root, result);
+    return result;
+  }
+};
---
+++ BinaryTree590.cpp
@@ -0,0 +1,46 @@
+#include <stddef.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val)
+  {
+    val = _val;
+  }
+
+  Node(int _val, vector<Node *> _children)
+  {
+    val = _val;
+    children = _children;
+  }
+};
+
+class Solution
+{
+public:
+  void traveral(Node *cur, vector<int> &result)
+  {
+    if (cur == NULL)
+      return;
+    for (auto n : cur->children)
+    {
+      traveral(n, result);
+    }
+    result.push_back(cur->val);
+  }
+
+  vector<int> preorder(Node *root)
+  {
+    vector<int> result;
+    traveral(root, result);
+    return result;
+  }
+};
