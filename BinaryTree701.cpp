3133eb25e6d87ebcbdf6174130193d6eb2a93cce
YUEQIN18 <qinyue12345@gmail.com>
2021-11-09 00:57:02 -0500

    11/08

---
+++ BinaryTree701.cpp
@@ -0,0 +1,57 @@
+#include <stddef.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+  TreeNode *insertIntoBST(TreeNode *root, int val)
+  {
+    if (root == nullptr)
+    {
+      TreeNode *node = new TreeNode(val);
+      return node;
+    }
+    if (val > root->val)
+      root->right = insertIntoBST(root->right, val);
+    if (val < root->val)
+      root->left = insertIntoBST(root->left, val);
+    return root;
+  }
+};
+
+/* 插入二叉搜索树 迭代法
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == NULL) {
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+        TreeNode* cur = root;
+        TreeNode* parent = root; // 这个很重要，需要记录上一个节点，否则无法赋值新节点
+        while (cur != NULL) {
+            parent = cur;
+            if (cur->val > val) cur = cur->left;
+            else cur = cur->right;
+        }
+        TreeNode* node = new TreeNode(val);
+        if (val < parent->val) parent->left = node;// 此时是用parent节点的进行赋值
+        else parent->right = node;
+        return root;
+    }
+};
+/*
