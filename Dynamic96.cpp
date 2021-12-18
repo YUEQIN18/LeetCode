feb854f76d6b2b305586cb60df4c54dba1cffe34
YUEQIN18 <qinyue12345@gmail.com>
2021-12-18 00:53:56 -0500

    12/17

---
+++ Dynamic96.cpp
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+using namespace std;
+
+class Solution
+{
+public:
+  int numTrees(int n)
+  {
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= i; j++)
+      {
+        dp[i] += dp[j - 1] * dp[i - j];
+      }
+    }
+    return dp[n];
+  }
+};
