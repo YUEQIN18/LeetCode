0f27e1dfcca6c4ee21e220fad4ddf04f590d207e
QINYUE <qinyue12345@gmail.com>
2023-09-20 20:32:00 +0800

    Update LinkedList

---
+++ Java/LinkdeList/Solution2095.java
@@ -0,0 +1,39 @@
+package LinkdeList;
+
+/**
+ * 2095. 删除链表的中间节点
+ * 给你一个链表的头节点 head 。删除 链表的 中间节点 ，并返回修改后的链表的头节点 head 。
+ * 长度为 n 链表的中间节点是从头数起第 ⌊n / 2⌋ 个节点（下标从 0 开始），其中 ⌊x⌋ 表示小于或等于 x 的最大整数。
+ * 对于 n = 1、2、3、4 和 5 的情况，中间节点的下标分别是 0、1、1、2 和 2 。
+ */
+public class Solution2095 {
+    // 双指针法遍历，可恶！我怎么没想到！ 当fast遍历到尽头的时候,slow刚好是要删除元素的前一个node
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode dummyHead = new ListNode(-1, head);
+        ListNode slow = dummyHead;
+        ListNode fast = dummyHead.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return dummyHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
