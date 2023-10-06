e8329ceeee5e0f4a481f32c0e8861c22d4cb886e
YUEQIN18 <qinyue12345@gmail.com>
2023-10-07 00:37:38 +0800

    Update Array

---
+++ Java/Array/Solution977.java
@@ -0,0 +1,20 @@
+package Array;
+
+class Solution977 {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+
+        for(int i = nums.length - 1; i >=0; i--){
+            if(nums[left]*nums[left] > nums[right]* nums[right]){
+                result[i] = nums[left]*nums[left];
+                left++;
+            }else{
+                result[i] = nums[right]* nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
