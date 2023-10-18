c01e0c747f3d1ac189c23be4afb93289277b7ed0
YUEQIN18 <qinyue12345@gmail.com>
2023-10-18 18:14:13 +0800

    Update Array

---
+++ Java/Array/Solution219.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 219. 存在重复元素 II
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，
+ * 满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+ */
+public class Solution219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if (map.containsKey(n) && Math.abs(i - map.get(n)) <= k) {
+                return true;
+            } else {
+                map.put(n, i);
+            }
+        }
+        return false;
+    }
+}
