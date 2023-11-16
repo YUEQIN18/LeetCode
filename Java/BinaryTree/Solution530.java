c743d2d7a9bf97c0b8587d21fbe4ddabc993bba2
YUEQIN18 <qinyue12345@gmail.com>
2023-11-16 18:18:45 +0800

    Update BinaryTree

---
+++ Java/BinaryTree/Solution530.java
@@ -0,0 +1,28 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 530. 二叉搜索树的最小绝对差
+ * 给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 。
+ * 差值是一个正数，其数值等于两值之差的绝对值。
+ */
+public class Solution530 {
+    private List<Integer> list = new ArrayList<>();
+    public int getMinimumDifference(TreeNode root) {
+        traversal(root);
+        if (list.size() <= 1) return 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) {
+            min = Math.min(min, list.get(i) - list.get(i-1));
+        }
+        return min;
+    }
+    private void traversal(TreeNode root) {
+        if (root == null) return;
+        traversal(root.left);
+        list.add(root.val);
+        traversal(root.right);
+    }
+}
