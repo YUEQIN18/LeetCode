2d355e891f9093e20e8a4df7b3b34c711c2993ec
YUEQIN18 <qinyue12345@gmail.com>
2023-11-24 18:20:30 +0800

    Update Graph

---
+++ Java/Graph/Solution200.java
@@ -0,0 +1,37 @@
+package Graph;
+
+/**
+ * 200. 岛屿数量
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 此外，你可以假设该网格的四条边均被水包围。
+ */
+public class Solution200 {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        // 遍历所有节点 找到陆地节点
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid, int r, int c) {
+        if (!inArea(grid, r, c)) return;
+        // 这个格子不是岛屿 直接返回
+        if (grid[r][c] != '1') return;
+        grid[r][c] = '2'; // 遍历过的格子标记为2
+
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
+    }
+    private boolean inArea(char[][] grid, int r, int c) {
+        return 0 <= r && r < grid.length && 0 <= c && c < grid[0].length;
+    }
+}
