d9749d9717e00ae6349347cecaf6f23442621bbf
YUEQIN18 <qinyue12345@gmail.com>
2024-03-26 16:22:07 +0800

    update Sort

--- Java/Sort/Solution215.java
+++ Java/Sort/Solution215.java
@@ -1,18 +1,23 @@
 package Sort;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Random;
 
 /**
  * @author qinyue
- * @create 2024-03-26 16:13:00
- * 912. 排序数组
+ * @create 2024-03-26 16:21:00
+ * 215. 数组中的第K个最大元素
+ * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+ * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
  */
-public class Solution912 {
+public class Solution215 {
     private static final Random RANDOM = new Random();
-    public int[] sortArray(int[] nums) {
+    public int findKthLargest(int[] nums, int k) {
         int len = nums.length;
         quickSort(nums, 0, len - 1);
-        return nums;
+        return nums[len - k];
     }
 
     private void quickSort(int[] nums, int left, int right) {
@@ -50,5 +55,25 @@ public class Solution912 {
         int temp = nums[index1];
         nums[index1] = nums[index2];
         nums[index2] = temp;
+    }
+
+    // 堆排序法（优先级队列）
+    public int findKthLargest2(int[] nums, int k) {
+        int len = nums.length;
+        // 使用一个长度为k的最小堆
+        PriorityQueue<Integer> heap = new PriorityQueue<>(k, Comparator.comparingInt(a -> a));
+        // 逐个插入堆
+        for (int i = 0; i < k; i++) {
+            heap.offer(nums[i]);
+        }
+        // 从k个元素开始 比堆顶元素大就入堆，这样堆中就保持了前k个大元素
+        for (int i = k; i < len; i++) {
+            int top = heap.peek();
+            if (nums[i] > top) {
+                heap.poll();
+                heap.offer(nums[i]);
+            }
+        }
+        return heap.peek();
     }
 }
